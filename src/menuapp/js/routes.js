26a0cd290385406129082dddcbf7b5b3663b2e87
vidzyu <dzyuba.victor@gmail.com>
2022-11-06 21:20:14 -0500

    Initial commit

---
+++ src/menuapp/js/routes.js
@@ -0,0 +1,49 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/menuapp/templates/categories.html',
+    controller: 'CategoriesController as categoriesCtrl',
+    resolve: {
+      categories: ['MenuDataService', function(MenuDataService) {
+        return MenuDataService.getAllCategories().then(function(response) {
+          return response.data;
+        });
+      }]
+    }
+  })
+
+  // Menu Items page
+  .state('items', {
+    url: '/{category}/items',
+    templateUrl: 'src/menuapp/templates/items.html',
+    controller: 'ItemsController as itemsCtrl',
+    resolve: {
+      items: ['MenuDataService', '$stateParams', function(MenuDataService, $stateParams) {
+            return MenuDataService.getItemsForCategory($stateParams.category).then(function(response) {
+              return response.data.menu_items;
+            });
+          }]
+      }
+  })
+
+
+
+  ;
+}
+
+})();
